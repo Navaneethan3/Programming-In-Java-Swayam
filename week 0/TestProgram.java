33377eaeac0e344af5cfe635abd6a50a9b171fe4
Nava <nava1982004@gmail.com>
2025-09-08 19:48:31 +0530

    Updated files to match required output

--- week 0/TestProgram.java
+++ week 0/TestProgram.java
@@ -1,6 +1,6 @@
 public class TestProgram{
    public static void main(String args[]){
-   System.out.println("We have successfully run a java program!!!");
+   System.out.println("We have successfully run a Java Program!!!");
    }
 } 
 
